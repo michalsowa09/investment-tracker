dc34156c3588552aa4294120870ba04151af2126
michalsowa09 <michalsowa9@o2.pl>
2026-05-13 12:27:38 +0200

    implement asser listing and total value calculation endpoints

--- app/database.py
+++ app/database.py
@@ -2,16 +2,16 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
+#Pobieram sobie adres bazy danych ze zmiennej środowiskowej
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@db:5432/investment_db")
 
 #Silnik - to on faktycznie wysyła dane do Postgresa.
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-#Fabryka sesji - każde wejście użytkownika na strone to nowa sesja.
+#Fabryka sesji - każde wejście użytkownika na strone to nowa sesja (taki zeszyt plus notatki).
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 #Funckja pomocnicza - daje dostęp do bazy w funkcjach.
-
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
--- app/main.py
+++ app/main.py
@@ -21,8 +21,46 @@ async def root():# To wtedy uruchamia mi tę funkcję.
 @app.post("/assets")
 async def create_asset(name: str, ticker: str, amount: float, price: float, db:
 AsyncSession = Depends(get_db)):
+    #Tworznie obiektu klasy asset
     new_asset = Asset(name = name, ticker = ticker, amount = amount, purchase_price = price)
+    #Dodaję to do sesji - planuję to zapisać.
     db.add(new_asset)
+    #Wysyłam dane do postgresa.
     await db.commit()
+    #Robię odświeżenie - czyli mówię pobierz z bazy to co zapisałem i uzupełnij mój obiekt o ewentualne braki.
     await db.refresh(new_asset)
+    #Zrwacam obiekt klasy Asset
     return new_asset
+
+#ENDPOINT DO POBIERANIA WSZYSTKICH DANYCH:
+
+@app.get("/assets")
+async def get_all_assets(db: AsyncSession = Depends(get_db)):
+    #tworzenie zapytania - pobranie wszystkiego z tabeli asset:
+    query = select(Asset)
+
+    #Wykonuje zapytanie w bazie danych:
+    result = await db.execute(query)
+    #Wyciąganie samych obiektów jako listę
+    assets_list = result.scalars().all()
+    return assets_list
+
+#ENDPOINT LICZĄCY ŁĄCZNĄ WARTOŚĆ ZAKUPÓW:
+@app.get("/total-value")
+async def get_total_value(db: AsyncSession = Depends(get_db)):
+    query = select(Asset)
+    #Wykonuje zapytanie do bazy danych:
+    result = await db.execute(query)
+    #Zwrócenie wyniku w postaci listy obiektów:
+    assets_list = result.scalars().all()
+
+    total_value = 0
+    for i in range(len(assets_list)):
+        total_value += assets_list[i].amount * assets_list[i].purchase_price
+
+    return {
+        "Suma_wydatków": total_value,
+        "Liczba_aktywów": len(assets_list)
+    }
+
+
