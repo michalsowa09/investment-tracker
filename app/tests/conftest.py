529382ddf1c3c2536f51dea0ede27f0ee4706813
michalsowa09 <michalsowa9@o2.pl>
2026-06-13 14:59:37 +0200

    ci: fix event loop scope mismatch in automated tests #6

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,12 +1,22 @@
 import pytest
+import asyncio
 from app.database import engine
 from app.models import Base
 
-# Ten dekorator przygotuje bazę danych przed testami
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
 @pytest.fixture(scope="session", autouse=True)
 async def setup_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
     yield
+
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
