87ff681983706ddb025da39e74ce330e159dd542
michalsowa09 <michalsowa9@o2.pl>
2026-06-13 15:06:53 +0200

    ci: fix scope mismatch

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,17 +1,22 @@
 import pytest
+import asyncio
 from app.database import engine
 from app.models import Base
 
 
-# Zmieniamy scope na "function" (domyślny) - to naprawi błąd ScopeMismatch
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
 async def setup_database():
-    # 1. Przed KAŻDYM testem tworzymy tabele od zera
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-    yield  # Tu uruchamia się test
+    yield
 
-    # 2. Po KAŻDYM teście czyścimy bazę, żeby następny miał czyste pole
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
